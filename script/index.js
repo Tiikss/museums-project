fed18053e3c704c5a8ee8f48d92e6901e92de9ed
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 21:09:17 +0200

    Ad dynamic loading

--- script/index.js
+++ script/index.js
@@ -1,3 +1,6 @@
+let lastId = 1;
+let museums = [];
+
 function filterFunction() {
     const input = document.getElementById("myInput");
     const filter = input.value.toUpperCase();
@@ -5,7 +8,6 @@ function filterFunction() {
     const a = div.getElementsByTagName("a");
     for (let i = 0; i < a.length; i++) {
         txtValue = a[i].textContent || a[i].innerText;
-        console.log(filter);
         if (filter == "") {
             a[i].style.display = "none";
         } else if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -13,6 +15,15 @@ function filterFunction() {
         } else {
             a[i].style.display = "none";
         }
+    }
+    if (filter.length == 0) {
+        const cardContainer = document.querySelector(".card-container");
+        const loadMoreButton = document.querySelector(".load-more-button");
+        console.log("FJSDGHAIJSGHFJESGHHDS");
+        cardContainer.innerHTML = "";
+        lastId = 1;
+        loadMuseums();
+        loadMoreButton.style.display = "block";
     }
 }
 
@@ -26,4 +37,77 @@ function chooseCity(e) {
     for (let i = 0; i < a.length; i++) {
         a[i].style.display = "none";
     }
+
+    loadMuseumsBySearch(input.value);
 }
+
+function createCard(data) {
+    return `
+    <div class="col-md-4 col-sm-6">
+        <div class="box">
+            <img src="${data.images[0]}" alt="${data.title}" />
+            <div class="box-content">
+                <h3 class="title">${data.title}</h3>
+                <span class="post">${data.city}</span>
+                <ul class="icon">
+                    <li>
+                        <a href="single-museum.html?id=${data.id}"
+                            ><i class="fa fa-search"></i
+                        ></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+function setValues(data) {
+    const cardContainer = document.querySelector(".card-container");
+    for (let i = lastId; i < lastId + 3; i++) {
+        cardContainer.innerHTML += createCard(data[i - 1]);
+    }
+    lastId += 3;
+}
+
+function loadMore() {
+    setValues(museums);
+}
+
+function loadMuseumsBySearch(city) {
+    const cardContainer = document.querySelector(".card-container");
+    const newMuseums = museums.filter((el) => el.city == city);
+    const loadMoreButton = document.querySelector(".load-more-button");
+    loadMoreButton.style.display = "none";
+    cardContainer.innerHTML = "";
+    newMuseums.forEach((museum) => {
+        cardContainer.innerHTML += createCard(museum);
+    });
+}
+
+function loadMuseums() {
+    fetch("./data/museums.json")
+        .then((response) => response.json())
+        .then((data) => {
+            museums = data.museums;
+            cities = data.museums.map((el) => el.city);
+            cities = [...new Set(cities)];
+            setCities(cities);
+            setValues(museums);
+        });
+}
+
+function createCity(city) {
+    return `
+    <a onclick="chooseCity(this)">${city}</a>
+    `;
+}
+
+function setCities(cities) {
+    const dropdown = document.getElementById("myDropdown");
+    for (let i = 0; i < cities.length; i++) {
+        dropdown.innerHTML += createCity(cities[i]);
+    }
+}
+
+loadMuseums();
