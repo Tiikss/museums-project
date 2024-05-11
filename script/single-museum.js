65106304efc91938edb0f99f84bee39a89e4fe5d
Jovan02 <jovannikolic2801@gmail.com>
2024-05-11 17:29:01 +0200

    Add loading data from json

--- script/single-museum.js
+++ script/single-museum.js
@@ -1,3 +1,6 @@
+const id = Number(window.location.search.split("=")[1]);
+console.log(id);
+
 const slider = function () {
     const slides = document.querySelectorAll(".carousel-img");
     const btnLeft = document.querySelector(".btn--left");
@@ -60,4 +63,39 @@ const slider = function () {
     });
 };
 
-slider();
+const setValues = (data) => {
+    const title = document.querySelector(".title");
+    title.textContent = data.title;
+
+    const info = document.querySelector(".info");
+
+    const infoText = document.createElement("p");
+    infoText.classList.add("museum-text");
+    infoText.textContent = data.description;
+
+    info.appendChild(infoText);
+
+    for (let i = 0; i < data.images.length; i++) {
+        const imageContainer = document.querySelector(".img-container");
+
+        const img = document.createElement("img");
+        img.src = data.images[i];
+        img.alt = "museum image";
+        img.classList.add("carousel-img");
+
+        imageContainer.appendChild(img);
+    }
+    slider();
+};
+
+const loadData = (id) => {
+    fetch("./data/museums.json")
+        .then((response) => response.json())
+        .then((data) => {
+            museum = data.museums.filter((el) => el.id == id);
+            console.log(museum[0]);
+            setValues(museum[0]);
+        });
+};
+
+loadData(id);
