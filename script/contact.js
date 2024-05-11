093f3403f2c11e5f8187ff7f90090627297bc858
Tiikss <trakocevic7@gmail.com>
2024-05-11 19:17:09 +0200

    Add modal

---
+++ script/contact.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const submitBtn = document.querySelector("#submit");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const messageField = document.querySelector("#message");
+const modalTitle = document.querySelector("#modal-title");
+const modalMessage = document.querySelector("#modal-message");
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = (event) => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    console.log(nameField.value, emailField.value, messageField.value);
+    if (nameField.value==="" || emailField.value==="" || messageField.value==="") {
+        modalTitle.innerHTML="Error!";
+        modalMessage.innerHTML="Please fill in all the fields.";
+    }
+    else if (nameField.value!=="" && emailField.value!=="" && messageField.value!==""){
+        modalTitle.innerHTML="Thank you!";
+        modalMessage.innerHTML="Successfully sent a message!";
+    }
+    event.preventDefault();
+};
+
+submitBtn.addEventListener("click", openModal)
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
